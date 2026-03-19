86bebabdb4de3a5e3f9ff5a50469e2747cb8f4f6
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-03-19 05:10:27 +0100

    Add files via upload

--- civil enginnering projects/tall building/building_elements_main.py
+++ civil enginnering projects/tall building/building_elements_main.py
@@ -1,19 +1,25 @@
-from modules.building_elements import stairs
+import pandas as pd
+from modules.building_elements import stairs,RC_column
+from modules.io import exptxt
 #stairs
 story_height = 3.06 # m
 vertical_step = 18 # cm
-horizontal_step = 25
+horizontal_step = 25 # cm
 (statement,story_height,stairs_height,vertical_step,n_stairs,
 horizontal_step,stairs_length,slope_angle,slope_angle_deg)=\
 stairs(story_height, vertical_step, horizontal_step)
- # export results
-with open("results.txt", "w") as f:
-    f.write(f"statement: {statement} \n")
-    f.write(f"Story height: {story_height} m\n")
-    f.write(f"Stairs height: {stairs_height} cm\n")
-    f.write(f"Vertical step: {vertical_step} cm\n")
-    f.write(f"Number of stairs: {n_stairs}\n")
-    f.write(f"Horizontal step: {horizontal_step} cm\n")
-    f.write(f"Stairs length: {stairs_length} cm\n")
-    f.write(f"Slope angle (rad): {slope_angle}\n") 
-    f.write(f"Slope angle (deg): {slope_angle_deg}\n") 
+T1 = pd.DataFrame({"statement": [statement],"story_height": [story_height],"stairs_height": [stairs_height],
+"vertical_step": [vertical_step],"n_stairs": [n_stairs],"horizontal_step": [horizontal_step],
+"stairs_length": [stairs_length],"slope_angle": [slope_angle],"slope_angle_deg": [slope_angle_deg]})
+fc28,fe = 25,400 #MN/m2 or MPA
+S,n= 13.23,10
+G_current_floor,G_roof_top = 665,802 #kg
+Q_current_floor,Q_roof_top,Q_ground_floor = 150,100,250 #kg
+Smaj,NG,NQ,Nu,alpha,Brmin=\
+RC_column(fc28,fe,S,n,G_current_floor,G_roof_top,Q_roof_top,Q_current_floor,Q_ground_floor)
+T2 = pd.DataFrame({"Smaj": [Smaj],"NG": [NG],"NQ": [NQ],
+"Nu": [Nu],"alpha": [alpha],"Brmin": [Brmin]})
+tables = [T1,T2]
+names = ["stairs","RC_column"]
+# export results
+exptxt(tables, names, "tall building/tall building.txt", 12)
