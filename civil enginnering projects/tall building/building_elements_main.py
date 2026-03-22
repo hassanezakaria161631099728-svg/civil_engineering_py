504bf3a7424d4e8e035d7e845a53bb8fa761fe48
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-03-23 00:22:06 +0100

    Add files via upload

--- civil enginnering projects/tall building/building_elements_main.py
+++ civil enginnering projects/tall building/building_elements_main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from modules.building_elements import stairs,RC_column
+from modules.building_elements import stairs,RC_column,RC_shear_force
 from modules.io import exptxt
 #stairs
 story_height = 3.06 # m
@@ -11,6 +11,7 @@ stairs(story_height, vertical_step, horizontal_step)
 T1 = pd.DataFrame({"statement": [statement],"story_height": [story_height],"stairs_height": [stairs_height],
 "vertical_step": [vertical_step],"n_stairs": [n_stairs],"horizontal_step": [horizontal_step],
 "stairs_length": [stairs_length],"slope_angle": [slope_angle],"slope_angle_deg": [slope_angle_deg]})
+#reinforced concrete column
 fc28,fe = 25,400 #MN/m2 or MPA
 S,n= 13.23,10
 G_current_floor,G_roof_top = 665,802 #kg
@@ -19,7 +20,13 @@ Smaj,NG,NQ,Nu,alpha,Brmin=\
 RC_column(fc28,fe,S,n,G_current_floor,G_roof_top,Q_roof_top,Q_current_floor,Q_ground_floor)
 T2 = pd.DataFrame({"Smaj": [Smaj],"NG": [NG],"NQ": [NQ],
 "Nu": [Nu],"alpha": [alpha],"Brmin": [Brmin]})
-tables = [T1,T2]
-names = ["stairs","RC_column"]
+#RC shear force units are in mm and N
+At,b,d = 452,250,400
+Vu_reduced = 179000
+ft28,up,down,stmin=\
+RC_shear_force(fe,At,alpha,b,Vu_reduced,fc28,d)
+T3 = pd.DataFrame({"ft28": [ft28],"up": [up],"down": [down],"stmin": [stmin]})
+tables = [T1,T2,T3]
+names = ["stairs","RC_column","RC_shear_force"]
 # export results
 exptxt(tables, names, "tall building/tall building.txt", 12)
