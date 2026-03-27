9a377768c9b16ed1225b4248b748529686e5718f
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-03-27 18:43:42 +0100

    Add files via upload

--- modules/building_elements.py
+++ modules/building_elements.py
@@ -17,16 +17,21 @@ def stairs(story_height, vertical_step, horizontal_step):
  return (statement,story_height,stairs_height,vertical_step,n_stairs,
  horizontal_step,stairs_length,slope_angle,slope_angle_deg)
 
-def RC_column(fc28,fe,S,n,G_current_floor,G_roof_top,Q_roof_top,
-Q_current_floor,Q_ground_floor):
-  Smaj = 1.1 * S #m
-  NG = 1.1 * ((n-1) * G_current_floor + G_roof_top) * Smaj / 100 #converting Kg to KN 
-  NQ = (Q_roof_top + Q_current_floor * (1+0.9+0.8+0.7+0.6+(n-7)*0.5)+ Q_ground_floor) * Smaj / 100 #KN
+def RC_column(fc28,fe,S,n,G_current_floor,G_roof_top,Q_roof_top,Q_current_floor):
+  Smaj = 1.15 * S #m
+  NG = 1.1 * (n * G_current_floor + G_roof_top) * Smaj / 100 #converting Kg to KN 
+  NQ = (Q_roof_top + Q_current_floor * (1+0.9+0.8+0.7+0.6+(n-5)*0.5)) * Smaj / 100 #KN
   Nu = (1.35 * NG + 1.5 * NQ) / 1000 # converting KN to MN
   alpha = 0.85 / 1.2
   Brmin = Nu / (alpha * (fc28 / (0.9 * 1.5) + 0.01 * fe / 1.15)) #m2
 # gamma c is 1.5 and gamma s is 1.15 
-  return Smaj,NG,NQ,Nu,alpha,Brmin
+  Nd = (NG + NQ) / 1000 
+  Bcmin = Nd / (0.3 * fc28)
+  amin = Bcmin**0.5
+  a = math.ceil(amin*20)/20
+  Bc = a*a # we have a rectangular column
+  Br = (a - 0.02)**2
+  return Smaj,NG,NQ,Nu,alpha,Brmin,Nd,Bcmin,amin,a,Bc,Br
 
 def RC_shear_force(fe,At,alpha,b,Vu_reduced,fc28,d): #units mm and N
   ft28 = 0.06 * fc28 + 0.6 
