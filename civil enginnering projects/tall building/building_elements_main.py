1544ac166b84eef7068adfd457bddef0d533f9f7
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-03-28 09:48:45 +0100

    Add files via upload

--- civil enginnering projects/tall building/building_elements_main.py
+++ civil enginnering projects/tall building/building_elements_main.py
@@ -12,12 +12,12 @@ T1 = pd.DataFrame({"statement": [statement],"story_height": [story_height],"stai
 "vertical_step": [vertical_step],"n_stairs": [n_stairs],"horizontal_step": [horizontal_step],
 "stairs_length": [stairs_length],"slope_angle": [slope_angle],"slope_angle_deg": [slope_angle_deg]})
 #reinforced concrete column
-fc28,fe = 25,400 #MN/m2 or MPA
+fc28,fe = 30,400 #MN/m2 or MPA
 S,n= 13.035,7
 G_current_floor,G_roof_top = 665,802 #kg
-Q_current_floor,Q_roof_top = 150,100 #kg
+Q_current_floor,Q_roof_top,Q_ground_floor = 150,100,0 #kg
 Smaj,NG,NQ,Nu,alpha,Brmin,Nd,Bcmin,amin,a,Bc,Br=\
-RC_column(fc28,fe,S,n,G_current_floor,G_roof_top,Q_roof_top,Q_current_floor)
+RC_column(fc28,fe,S,n,G_current_floor,G_roof_top,Q_roof_top,Q_current_floor,Q_ground_floor)
 T2 = pd.DataFrame({"Smaj": [Smaj],"NG": [NG],"NQ": [NQ],"Nu": [Nu],"alpha": [alpha],
 "Brmin": [Brmin],"Nd": [Nd],"Bcmin": [Bcmin],"amin": [amin],"a": [a],"Bc": [Bc],"Br": [Br]})
 #RC shear force units are in mm and N
