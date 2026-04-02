b1e0f7189b5a8c5d8e6b32e0519383f653056787
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-04-02 16:53:36 +0100

    Add files via upload

--- civil enginnering projects/tall building/building_elements_main.py
+++ civil enginnering projects/tall building/building_elements_main.py
@@ -11,17 +11,29 @@ stairs(story_height, vertical_step, horizontal_step)
 T1 = pd.DataFrame({"statement": [statement],"story_height": [story_height],"stairs_height": [stairs_height],
 "vertical_step": [vertical_step],"n_stairs": [n_stairs],"horizontal_step": [horizontal_step],
 "stairs_length": [stairs_length],"slope_angle": [slope_angle],"slope_angle_deg": [slope_angle_deg]})
+
 #reinforced concrete column
-fc28,fe = 30,400 #MN/m2 or MPA
-S,n= 13.035,7
-G_current_floor,G_roof_top = 665,802 #kg
-Q_current_floor,Q_roof_top,Q_ground_floor = 150,100,0 #kg
+ # number of floors above the base level
+ #in general case the base level isn't necessary the ground floor but the last underneath floor
+ # the last floor under the surface of soil
+nSF = 7 # number of floors above the soil surface
+nUF = 1 # number of floors under the soil surface
+n = nSF + nUF  #total number of floors above the base level we add 1 the ground floor and we 
+ # remove the last underneath floor which is the base floor  
+fc28,fe = 25,400
+S = 13.035
+# dead loads G and live loads Q
+GRT,GCF = 802,665
+QRT,QCF = 100,150
 Smaj,NG,NQ,Nu,alpha,Brmin,Nd,Bcmin,amin,a,Bc,Br=\
-RC_column(fc28,fe,S,n,G_current_floor,G_roof_top,Q_roof_top,Q_current_floor,Q_ground_floor)
-T2 = pd.DataFrame({"Smaj": [Smaj],"NG": [NG],"NQ": [NQ],"Nu": [Nu],"alpha": [alpha],
-"Brmin": [Brmin],"Nd": [Nd],"Bcmin": [Bcmin],"amin": [amin],"a": [a],"Bc": [Bc],"Br": [Br]})
+RC_column(fc28,fe,S,n,GCF,GRT,QRT,QCF)
+T2 = pd.DataFrame({"Smaj": Smaj.reshape(-1),"NG": NG.reshape(-1),"NQ": NQ.reshape(-1),
+"Nu": Nu.reshape(-1),"alpha": alpha.reshape(-1),"Brmin": Brmin.reshape(-1),"Nd": Nd.reshape(-1),
+"Bcmin": Bcmin.reshape(-1),"amin": amin.reshape(-1),"a": a.reshape(-1),
+"Bc": Bc.reshape(-1),"Br": Br.reshape(-1)})
+
 #RC shear force units are in mm and N
-At,b,d = 452,250,400
+At,b,d,alpha = 452,250,400,0.85/1.2
 Vu_reduced = 179000
 ft28,up,down,stmin=\
 RC_shear_force(fe,At,alpha,b,Vu_reduced,fc28,d)
