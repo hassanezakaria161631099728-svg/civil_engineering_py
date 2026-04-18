792a6c52fbfafce02a48a8ccc9d385f3fba8195c
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-04-18 20:21:38 +0100

    Add files via upload

--- modules/building_elements.py
+++ modules/building_elements.py
@@ -118,27 +118,34 @@ def shape_geometry_attributes(a, b, xg, yg):
  return T_scalar, T_vec
 
 def sectorial(T_scalar1,T_scalar2,T_scalar3,T_scalar4,T_scalar5):
-#X axis
+# we define geometrical attributes for reinforced concrete walls
+#inertia X axis
  Ix_total1 = T_scalar1["Ix_total"].iloc[0]
  Ix_total2 = T_scalar2["Ix_total"].iloc[0]
  Ix_total3 = T_scalar3["Ix_total"].iloc[0]
  Ix_total4 = T_scalar4["Ix_total"].iloc[0]
  Ix_total5 = T_scalar5["Ix_total"].iloc[0]
 
-#Y axis
+#inertia Y axis
  Iy_total1 = T_scalar1["Iy_total"].iloc[0]
  Iy_total2 = T_scalar2["Iy_total"].iloc[0]
  Iy_total3 = T_scalar3["Iy_total"].iloc[0]
  Iy_total4 = T_scalar4["Iy_total"].iloc[0]
  Iy_total5 = T_scalar5["Iy_total"].iloc[0]
+# surfaces 
+ A_total1 = T_scalar1["A_total"].iloc[0]
+ A_total2 = T_scalar2["A_total"].iloc[0]
+ A_total3 = T_scalar3["A_total"].iloc[0]
+ A_total4 = T_scalar4["A_total"].iloc[0]
+ A_total5 = T_scalar5["A_total"].iloc[0]
  n = 16
 # inertia vectors
- Ix_total,Iy_total = np.zeros((n)), np.zeros((n))
- Ix_total[0:4], Iy_total[0:4] = Ix_total1, Iy_total1
- Ix_total[4:8], Iy_total[4:8] = Ix_total2, Iy_total2
- Ix_total[8:10], Iy_total[8:10] = Ix_total3, Iy_total3
- Ix_total[10:14], Iy_total[10:14] = Ix_total4, Iy_total4
- Ix_total[14:16], Iy_total[14:16] = Ix_total5, Iy_total5
+ Ix_total,Iy_total,A_total = np.zeros((n)), np.zeros((n)), np.zeros((n))
+ Ix_total[0:4], Iy_total[0:4], A_total[0:4] = Ix_total1, Iy_total1, A_total1 
+ Ix_total[4:8], Iy_total[4:8], A_total[4:8] = Ix_total2, Iy_total2, A_total2
+ Ix_total[8:10], Iy_total[8:10], A_total[8:10] = Ix_total3, Iy_total3, A_total3
+ Ix_total[10:14], Iy_total[10:14], A_total[10:14] = Ix_total4, Iy_total4, A_total4
+ Ix_total[14:16], Iy_total[14:16], A_total[14:16] = Ix_total5, Iy_total5, A_total5
 # centre of mass vectors
  xg1 = T_scalar1["xg_global"].iloc[0]
  xg4 = T_scalar1["xg_global"].iloc[0]
@@ -160,7 +167,7 @@ def sectorial(T_scalar1,T_scalar2,T_scalar3,T_scalar4,T_scalar5):
  Ly/2,Ly/2                                #i=14:16 
  ]
 
- Ix_scalar,Iy_scalar = np.sum(Ix_total),np.sum(Iy_total)
+ A_scalar,Ix_scalar,Iy_scalar = np.sum(A_total),np.sum(Ix_total),np.sum(Iy_total)
 
  #global torsion center
  XC = np.sum(Ix_total * X) / Ix_scalar
@@ -174,12 +181,32 @@ def sectorial(T_scalar1,T_scalar2,T_scalar3,T_scalar4,T_scalar5):
 
  RC_walls = ["RC_wall1","RC_wall2","RC_wall3","RC_wall4","RC_wall5","RC_wall6","RC_wall7","RC_wall8",
  "RC_wall9","RC_wall10","RC_wall11","RC_wall12","RC_wall13","RC_wall14","RC_wall15","RC_wall16"]
- T_sectorial = pd.DataFrame({"RC_walls": RC_walls,"Ix": Ix_total,"Iy": Iy_total,"X": X,"Y": Y,
+ T_sectorial = pd.DataFrame({"RC_walls": RC_walls,"A": A_total,"Ix": Ix_total,"Iy": Iy_total,"X": X,"Y": Y,
  "dx": dx,"dy": dy,"Iw":Iw_vec})
- T_sectorial_scalar = pd.DataFrame({"geometry_attribute": "value","Ix": [Ix_scalar],"Iy": [Iy_scalar],
- "XC": [XC],"YC": [YC],"Iw":Iw_scalar})
+ T_sectorial_scalar = pd.DataFrame({"geometry_attribute": "value","A": [A_scalar],"Ix": [Ix_scalar],
+ "Iy": [Iy_scalar],"XC": [XC],"YC": [YC],"Iw":Iw_scalar})
 
  return T_sectorial, T_sectorial_scalar
+
+def building_masses(geometry,Lx,Ly,G_RTB,h_story,h_beam,a_RC_column,n_RC_columns,DC):
+ geometry_attributes = geometry["sectorial_attributes_scalars"]
+ geometry_attributes["A"] = geometry_attributes["A"].astype(float)
+ A_RC_walls = geometry_attributes["A"].iloc[0]
+ #roof top barricade 
+ L_RTB = (Lx + Ly) * 2 #m
+ m_RTB = G_RTB * L_RTB / 1000
+ T_RTB = pd.DataFrame({"G_kg/m": [G_RTB],"L_m": [L_RTB],"m_tonnes": [m_RTB]})
+ # RC walls
+ h_RC_wall = h_story - h_beam
+ v_RC_walls = A_RC_walls * h_RC_wall          
+ m_RC_walls = v_RC_walls * DC           
+ T_RC_walls = pd.DataFrame({"height_m": [h_RC_wall],"volume_m3": [v_RC_walls],"m_tonnes": [m_RC_walls]})
+ # RC columns
+ S_RC_column = a_RC_column * a_RC_column
+ m_RC_columns = h_story * n_RC_columns * S_RC_column *  DC 
+ T_RC_columns = pd.DataFrame({"n_columns": [n_RC_columns],"a_m": [a_RC_column],"S_m2": [S_RC_column],
+ "m_tonnes": [m_RC_columns]})
+ return T_RTB,T_RC_walls,T_RC_columns
 
 def factor(h,E,I):
  f= h**3 / (6 * E * I) #m/KN
