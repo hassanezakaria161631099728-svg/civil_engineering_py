99e8f78a5739fe7463d39052e2137480b7539857
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-04-21 12:58:18 +0100

    Add files via upload

--- civil enginnering projects/tall building/tall_building.py
+++ civil enginnering projects/tall building/tall_building.py
@@ -1,24 +1,35 @@
 import sys
 import numpy as np
 import pandas as pd
-from modules.building_elements import (RC_column,shape_geometry_attributes,sectorial,
-building_masses,dynamic1,dynamic2)
+from modules.building_elements import (stairs,RC_column,RC_shear_force,shape_geometry_attributes,
+sectorial,building_masses,dynamic1,dynamic2)
 from modules.io import matrix_to_table,export_matrices_txt2,exptxt,read_tables_txt3
 
-def columns():
- print("Running case 1: columns analysis")
+def elements():
+ print("Running case 1: elements")
  # your code here
+ #stairs
+ story_height = 3.06 # m
+ vertical_step = 18 # cm
+ horizontal_step = 25 # cm
+ (statement,story_height,stairs_height,vertical_step,n_stairs,
+ horizontal_step,stairs_length,slope_angle,slope_angle_deg)=\
+ stairs(story_height, vertical_step, horizontal_step)
+ T1 = pd.DataFrame({"statement": [statement],"story_height": [story_height],"stairs_height": [stairs_height],
+ "vertical_step": [vertical_step],"n_stairs": [n_stairs],"horizontal_step": [horizontal_step],
+ "stairs_length": [stairs_length],"slope_angle": [slope_angle],"slope_angle_deg": [slope_angle_deg]})
+ #reinforced concrete column
  # number of floors above the base level
  #in general case the base level isn't necessary the ground floor but the last underneath floor
  # the last floor under the surface of soil
- nSF = 0 # number of floors above the soil surface
+ nSF = 9 # number of floors above the soil surface
  nUF = 0 # number of floors under the soil surface
  n = nSF + nUF  #total number of floors above the base level we add 1 the ground floor and we 
  # remove the last underneath floor which is the base floor  
- fc28,fe = 0,0
- S = 0
-# dead loads G and live loads Q
- GRT,GCF = 797,665
+ fc28,fe = 30,500
+ S = 20.36
+ # dead loads G and live loads Q
+ GRT,GCF = 787,665
  QRT,QCF = 100,150
  Smaj,NG,NQ,Nu,alpha,Brmin,Nd,Bcmin,amin,a,Bc,Br=\
  RC_column(fc28,fe,S,n,GCF,GRT,QRT,QCF)
@@ -26,43 +37,46 @@ def columns():
  "Nu": Nu.reshape(-1),"alpha": alpha.reshape(-1),"Brmin": Brmin.reshape(-1),"Nd": Nd.reshape(-1),
  "Bcmin": Bcmin.reshape(-1),"amin": amin.reshape(-1),"a": a.reshape(-1),
  "Bc": Bc.reshape(-1),"Br": Br.reshape(-1)})
-
-#showing on terminal
-# print(T2)  
-# export results
- tables = [T2]
- names = ["RC_column"]
- exptxt(tables, names, "tall building/RC column.txt", 12)
+ #RC shear force units are in mm and N
+ At,b,d,alpha = 452,250,500,0.85/1.2
+ Vu_reduced = 179000
+ ft28,up,down,stmin=\
+ RC_shear_force(fe,At,alpha,b,Vu_reduced,fc28,d)
+ T3 = pd.DataFrame({"ft28": [ft28],"up": [up],"down": [down],"stmin": [stmin]})
+ # export results
+ tables = [T1,T2,T3]
+ names = ["stairs","RC_column","RC_shear_force"]
+ exptxt(tables, names, "tall building/elements.txt", 12)
 
 def geometry():
  print("Running case 2: geometry_attributes")
  # your code here
  #shape1
- a1 = [0]
- b1 = [0]
- xg1 = [0]
- yg1 = [0]
+ a1 = [0.4, 0.2, 0.4]
+ b1 = [0.4, 3.8, 0.4]
+ xg1 = [0.2, 0.2, 0.2]
+ yg1 = [0.2, 2.3, 4.4]
  T_scalar1, T_vec1 = shape_geometry_attributes(a1, b1, xg1, yg1)
 
  #shape2
- a2 = [0]
- b2 = [0]
- xg2 = [0]
- yg2 = [0]
+ a2 = [0.4, 0.2]
+ b2 = [0.4, 2.3]
+ xg2 = [0.2, 0.2]
+ yg2 = [0.2, 1.55]
  T_scalar2, T_vec2 = shape_geometry_attributes(a2, b2, xg2, yg2)
 
  #shape3
- a3 = [0]
- b3 = [0]
- xg3 = [0]
- yg3 = [0]
+ a3 = [0.4, 2.1]
+ b3 = [0.4, 0.2]
+ xg3 = [0.2, 1.45]
+ yg3 = [0.2, 0.2]
  T_scalar3, T_vec3 = shape_geometry_attributes(a3, b3, xg3, yg3)
 
  #shape4
- a4 = [0]
- b4 = [0]
- xg4 = [0]
- yg4 = [0]
+ a4 = [5.75]
+ b4 = [0.2]
+ xg4 = [2.875]
+ yg4 = [0.1]
  T_scalar4, T_vec4 = shape_geometry_attributes(a4, b4, xg4, yg4)
 
  # general scheme RC walls coordinates
@@ -71,12 +85,20 @@ def geometry():
  yg1g = T_scalar1["yg_global"].iloc[0]
  yg2g = T_scalar2["yg_global"].iloc[0]
 
- Lx,Ly = 0,0 
+ Lx,Ly = 21.77,15.7 
  # X
- X = [0]
+ X = [0.2, Lx-0.2, 0.2, Lx-0.2,                #i=0:4
+ 4.7, Lx-4.7, 4.7, Lx-4.7,                     #i=4:8
+ 4.5+xg3g, Lx-4.5-xg3g, 4.5+xg3g, Lx-4.5-xg3g, #i=8:12
+ 4.7, Lx-4.7, 4.7, Lx-4.7,                     #i=12:16     
+]
 
  # Y
- Y = []
+ Y = [1+yg1g, 1+yg1g, Ly-1-yg1g, Ly-1-yg1g,    #i=0:4
+ 1+yg2g, 1+yg2g, Ly-1-yg2g, Ly-1-yg2g,         #i=4:8
+ 5.4, 5.4, Ly-5.4, Ly-5.4,                     #i=8:12
+ 0.1, 0.1, Ly-0.1, Ly-0.1,                     #i=12:16     
+]
 
  T_sectorial,T_sectorial_scalars = sectorial(T_scalar1,T_scalar2,T_scalar3,T_scalar4,X,Y)
 
@@ -91,13 +113,13 @@ def masses():
  print("running case 3: masses")
  #your code here
  geometry = read_tables_txt3("tall building/geometry.txt")
- Lx,Ly = 0,0 #building dimension x y
- h_story,h_beam = 0,0 
+ Lx,Ly = 23.05,19.25 #building dimension x y
+ h_story,h_beam = 3.06,0.4 
  DC = 2.5 #concrete density
  # roof top barricade
  G_RTB = 211.1 #kg/m
  #RC columns
- n_RC_columns,a_RC_column = 0,0
+ n_RC_columns,a_RC_column = 48,0.45
  T_RTB,T_RC_walls,T_RC_columns = \
  building_masses(geometry,Lx,Ly,G_RTB,h_story,h_beam,a_RC_column,n_RC_columns,DC)
 # export results
@@ -108,21 +130,26 @@ def masses():
 def dynamic():
  print("Running case 4: dynamic analysis")
  # your code here
- n = 0 #number of floors above the base floor
+ n = 8 #number of floors above the base floor
  M1, M2, M3 = 606.342, 603.945, 577.434 #masses
+ Lx, Ly = 22.6, 18.8
  # SA and mass matrices and Elasticity module
- SA, M, E, TSA, TM = dynamic1(n,M1,M2,M3)
- h = 0 #m story height
- Ix, Iy = 0, 0
+ h = 3.06 #m story height
+ # case there is only "with bricks" or "no bricks"
+ case = "with bricks"
+ SA, M, MR, E, TSA, TM, T_imperial = dynamic1(n,M1,M2,M3,h,case,Lx,Ly)
+ Ix, Iy, Iw = 15.49, 22.3166, 1964.139
  fx, TSx, TDx, eigen_valuesx, Teigen_vectors, periods_x = dynamic2(h,E,Iy,SA,M) 
  fy, TSy, TDy, eigen_valuesy, _, periods_y = dynamic2(h,E,Ix,SA,M) 
- names = ["SA","M","Sx","Dx","eigen_vectors","Sy","Dy"]
+ fw, TSw, TDw, eigen_valuesw, _, periods_y = dynamic2(h,E,Iw,SA,MR) 
+ names = ["SA","M","Sx","Dx","eigen_vectors","Sy","Dy","MR","Sw","Dw"]
  matrices = [TSA, TM, TSx, TDx, Teigen_vectors, TSy, TDy]
 # float_format = "{:.3f}".format
  export_matrices_txt2(matrices, names, "tall building/dynamic.txt")
  T = pd.DataFrame({"eigen_valuesx": eigen_valuesx,"eigen_valuesy": eigen_valuesy,
  "periods_x_s": periods_x,"periods_y_s": periods_y})
- T2 = pd.DataFrame({"Elasticity_module": [E],"Iy": [Iy],"Ix": [Ix],"fx": [fx],"fy": [fy]})
+ T2 = pd.DataFrame({"Elasticity_module": [E],"Iy": [Iy],"Ix": [Ix],"fx": [fx],"fy": [fy],
+ "T_imperial":[T_imperial]})
 # export results
  tables = [T,T2]
  names = ["periods_secondes","scalars"]
@@ -131,7 +158,7 @@ def dynamic():
 def test():
  print("Running case 2: testing")
  # your code here
- n = 0
+ n = 16
  Ix_total,Iy_total = np.zeros((n)), np.zeros((n))
  Ix_total[0:4], Iy_total[0:4] = 1, 1
  Ix_total[4:8], Iy_total[4:8] = 2, 2
@@ -147,8 +174,8 @@ if __name__ == "__main__":
     else:
         command = sys.argv[1]
 
-        if command == "columns":
-            columns()
+        if command == "elements":
+            elements()
         elif command == "geometry":
             geometry()
         elif command == "masses":
